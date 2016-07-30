544f762bc2b507798bf104e43fee1158a7b7ebec
Sean Flannery <FlanMan97@users.noreply.github.com>
2016-07-30 19:43:35 -0400

    Updated Date

--- src/Question5GUI.java
+++ src/Question5GUI.java
@@ -105,7 +105,7 @@ public class Question5GUI extends javax.swing.JFrame {
 			}
 		});
 		jLabel3.setFont(new java.awt.Font("Copperplate Gothic Bold", 1, 36));
-		jLabel3.setText("CS Bridge Course 2015");
+		jLabel3.setText("CS Bridge Course 2016");
 
 		// Following code takes care of the alignment of the GUI so that
 		// components are placed at the appropriate positions
